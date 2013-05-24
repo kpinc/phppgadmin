90a528afc4c77f364c5c3410bfb88aaebeb08f05
Karl O. Pinc <kop@meme.com>
2013-05-24 12:14:09 -0500

    Processes: Enhance to support supression of internal PPA processes.
    As yet no interface to frob this.

--- classes/database/Postgres.php
+++ classes/database/Postgres.php
@@ -7193,23 +7193,38 @@ class Postgres extends ADODB_base {
 
 	/**
 	 * Returns all available process information.
+	 * @param $filter             (boolean) When true filter out the process
+	 * 		  					  used to query the processes.
 	 * @param $database (optional) Find only connections to specified database
 	 * @return A recordset
 	 */
-	function getProcesses($database = null) {
+	function getProcesses($filter, $database = null) {
 		if ($database === null)
-			$sql = "SELECT datname, usename, pid, waiting, state_change as query_start,
-                  case when state='idle in transaction' then '<IDLE> in transaction' when state = 'idle' then '<IDLE>' else query end as query 
-				FROM pg_catalog.pg_stat_activity
-				ORDER BY datname, usename, pid";
+		   	$whereclause = '';
 		else {
 			$this->clean($database);
-			$sql = "SELECT datname, usename, pid, waiting, state_change as query_start,
-                  case when state='idle in transaction' then '<IDLE> in transaction' when state = 'idle' then '<IDLE>' else query end as query 
-				FROM pg_catalog.pg_stat_activity
-				WHERE datname='{$database}'
-				ORDER BY usename, pid";
+			$whereclause = "datname='{$database}'";
 		}
+
+		if ($filter) {
+		   	$hash = sha1(rand());
+			$hashcol = "'{$hash}' AS hash,";
+
+		   	if ($whereclause !== '')
+			   	$whereclause .= ' AND ';
+			$whereclause .= "POSITION('{$hash}' IN query) = 0";
+		} else {
+		  	$hashcol = '';
+		}
+
+		if ($whereclause !== '')
+		   	$whereclause = "WHERE ${whereclause}";
+
+		$sql = "SELECT {$hashcol} datname, usename, pid, waiting, state_change as query_start,
+                 case when state='idle in transaction' then '<IDLE> in transaction' when state = 'idle' then '<IDLE>' else query end as query 
+			FROM pg_catalog.pg_stat_activity
+			{$whereclause}
+			ORDER BY datname, usename, pid";
 
 		return $this->selectSet($sql);
 	}
