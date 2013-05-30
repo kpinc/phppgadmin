512379240e678716e4d0ff4a36703d699f081771
Karl O. Pinc <kop@meme.com>
2013-05-30 08:59:06 -0500

    Processes: Improve error message when webserver cannot be reached.
    Distinguish webserver from PG server.

--- lang/english.php
+++ lang/english.php
@@ -198,7 +198,7 @@
 	$lang['strnoserversupplied'] = 'No server supplied!';
 	$lang['strbadpgdumppath'] = 'Export error: Failed to execute pg_dump (given path in your conf/config.inc.php : %s). Please, fix this path in your configuration and relog.';
 	$lang['strbadpgdumpallpath'] = 'Export error: Failed to execute pg_dumpall (given path in your conf/config.inc.php : %s). Please, fix this path in your configuration and relog.';
-	$lang['strconnectionfail'] = 'Can not connect to server.';
+	$lang['strconnectionfail'] = 'Can not connect to webserver.';
 
 	// Tables
 	$lang['strtable'] = 'Table';
